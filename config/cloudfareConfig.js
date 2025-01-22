d98b907b17082548019bf155266e85e614838c94
faiq157 <faiqa5325@gmail.com>
2025-01-22 14:27:26 +0500

    tracking end point added

--- config/cloudfareConfig.js
+++ config/cloudfareConfig.js
@@ -3,7 +3,6 @@ const s3 = new AWS.S3({
   endpoint: new AWS.Endpoint(process.env.AWS_S3_ENDPOINT),
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
   token: process.env.AWS_TOKEN,
   signatureVersion: process.env.AWS_SIGNATURE_VERSION,
 });
