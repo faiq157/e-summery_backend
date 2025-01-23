484ea8455650db99d0059436de3f2f5cbbaf566a
faiq157 <faiqa5325@gmail.com>
2025-01-23 22:18:56 +0500

    completed status add for establisment

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header("Authorization")?.replace("Bearer ", "");
+  const token = req.header("Authorization")
 
   if (!token) {
     return res.status(401).json({ msg: "No token, authorization denied" });
--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const UserSchema = new mongoose.Schema({
   fullname: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  token: { type: String, required: true },
    role: {
     type: String,
     required:true,
