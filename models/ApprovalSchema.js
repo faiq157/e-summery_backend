82ad7db2b67efef257c9948163507f881997f920
faiq157 <faiqa5325@gmail.com>
2025-01-25 22:19:18 +0500

    approval serivce added

---
+++ models/ApprovalSchema.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const ApprovalSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    pdfUrl: {
+      type: String,
+      required: true,
+    },
+      sentTo: {
+      type: [mongoose.Schema.Types.ObjectId], // Array of user IDs
+      ref: "User", // Reference to the User model
+      default: [], // Default is an empty array
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("approvalSchema", ApprovalSchema);
